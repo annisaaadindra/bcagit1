379b97b3bf1d7faf1cbb4d9002bf0bf19c70790b
Herdhiantoko Fathani <herdhiantokofathani@gmail.com>
2020-08-18 10:38:31 +0700

    added function GanjilGenap

--- gitTim1.js
+++ gitTim1.js
@@ -1,43 +1,54 @@
 //MENCARI RUTE TERDEKAT DENGAN MEMANFAATKAN GRAPH & ALGORITMA GREEDY
 
 //variabel graph('node asal': ('node tujuan': weight))
-var rute = {'A': {'C': 4, 'G': 9},
-'G': {'E': 6},
-'C': {'D': 6, 'H': 12},
-'D': {'E': 7},
-'H': {'F': 15},
-'E': {'F': 8},
-'F': {'B': 5}};
+var rute = {
+  A: { C: 4, G: 9 },
+  G: { E: 6 },
+  C: { D: 6, H: 12 },
+  D: { E: 7 },
+  H: { F: 15 },
+  E: { F: 8 },
+  F: { B: 5 },
+};
 
 //fungsi shortest path using greedy algorithm
-function greedy_shortest(graph, source, dest)//parameter tdd pilihan rute, lokasi berangkat dan lokasi tujuan
-{
-    result = [];
-    result.push(source);
-    var current_node ;   
-    
-    while (dest !== result[result.length-1]) 
-    {
-        current_node = result[result.length-1];
-        var check_key = dest in graph[current_node];//mencegat pencarian local max jika titik selanjutnya adalah dest
-        if(check_key)
-        {
-            result.push(dest);
-            break;
-        }
+function greedy_shortest(graph, source, dest) {
+  //parameter tdd pilihan rute, lokasi berangkat dan lokasi tujuan
+  result = [];
+  result.push(source);
+  var current_node;
 
-        var [localmax] = Object.entries(graph[current_node]).sort(([ ,v1], [ ,v2]) => v1 - v2);
-        var node_max = localmax[0];
-        result.push(node_max);
+  while (dest !== result[result.length - 1]) {
+    current_node = result[result.length - 1];
+    var check_key = dest in graph[current_node]; //mencegat pencarian local max jika titik selanjutnya adalah dest
+    if (check_key) {
+      result.push(dest);
+      break;
     }
 
-    console.log(result);
+    var [localmax] = Object.entries(graph[current_node]).sort(
+      ([, v1], [, v2]) => v1 - v2
+    );
+    var node_max = localmax[0];
+    result.push(node_max);
+  }
+
+  console.log(result);
 }
 
-greedy_shortest(rute, 'A', 'H');
+// Function by Herdhiantoko
+function ganjilGenap(value) {
+  if (value % 2 == 0) {
+    console.log("Genap");
+  } else {
+    console.log("Ganjil");
+  }
+}
+
+greedy_shortest(rute, "A", "H");
 
 //ADD BY YUNDA
-var nilai = [5,7,1,6,8,10,22,13,7,8];
+var nilai = [5, 7, 1, 6, 8, 10, 22, 13, 7, 8];
 var max_ = Math.max.apply(Math, nilai);
 var min_ = Math.min.apply(Math, nilai);
 
