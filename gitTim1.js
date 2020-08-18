eb0626c9783e96d26debaaf18a2420b74fde5ad4
annisaaadindra <annisaaadindra@gmail.com>
2020-08-18 10:23:26 +0700

    first file

---
+++ gitTim1.js
@@ -0,0 +1,37 @@
+//MENCARI RUTE TERDEKAT DENGAN MEMANFAATKAN GRAPH & ALGORITMA GREEDY
+
+//variabel graph('node asal': ('node tujuan': weight))
+var rute = {'A': {'C': 4, 'G': 9},
+'G': {'E': 6},
+'C': {'D': 6, 'H': 12},
+'D': {'E': 7},
+'H': {'F': 15},
+'E': {'F': 8},
+'F': {'B': 5}};
+
+//fungsi shortest path using greedy algorithm
+function greedy_shortest(graph, source, dest)//parameter tdd pilihan rute, lokasi berangkat dan lokasi tujuan
+{
+    result = [];
+    result.push(source);
+    var current_node ;   
+    
+    while (dest !== result[result.length-1]) 
+    {
+        current_node = result[result.length-1];
+        var check_key = dest in graph[current_node];//mencegat pencarian local max jika titik selanjutnya adalah dest
+        if(check_key)
+        {
+            result.push(dest);
+            break;
+        }
+
+        var [localmax] = Object.entries(graph[current_node]).sort(([ ,v1], [ ,v2]) => v1 - v2);
+        var node_max = localmax[0];
+        result.push(node_max);
+    }
+
+    console.log(result);
+}
+
+greedy_shortest(rute, 'A', 'H');
